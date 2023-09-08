2e5fdfcef8b5d3e1b0a89a0e8666c441c00861fa
sisiicecream <anniebhee152gmail.com>
2023-09-08 17:46:59 +0100

    added endpoint for handling GET requests and returning JSON data

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, jsonify
+import datetime
+
+app = Flask(__name__)
+
+@app.route('/get-specific-info')
+def get_specific_info():
+    # Get the query parameters
+    slack_name = request.args.get('slack_name')
+    track = request.args.get('track')
+
+    # Get the current day of the week
+    current_day = datetime.datetime.utcnow().strftime('%A')
+
+    # Get the current UTC time
+    utc_time = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
+
+    # Get the GitHub file URL and GitHub repo URL (replace with your own URLs)
+    github_file_url = "https://github.com/username/repo/blob/main/file_name.ext"
+    github_repo_url = "https://github.com/username/repo"
+
+    # Prepare the response JSON
+    data = {
+        "slack_name": slack_name,
+        "current_day": current_day,
+        "utc_time": utc_time,
+        "track": track,
+        "github_file_url": github_file_url,
+        "github_repo_url": github_repo_url,
+        "status_code": 200
+    }
+
+    return jsonify(response_data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
